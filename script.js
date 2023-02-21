e267a7435a35046f91c1b45000c3ba10f3860141
kanybekova eleonora <eelya250@gmail.com>
2023-02-21 15:03:29 +0600

    added and changed the text with java

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ example2.textContent = "hello world";
 console.log(example2);
 
 let example3 = document.querySelector(".example-3");
-example3.textContent = "i'm so tired";
+example3.textContent = example3.textContent + "bye"; //добавить текст
 console.log(example3);
 
 let example4 = document.querySelector("#example-4");
-example4.textContent = "don't care";
+example4.textContent += "don't care"; // короткая версия добавить текст
 console.log(example4);
 
 let example5 = document.querySelector("p:nth-child(4)");
-example5.textContent = "i'm good ";
+example5.textContent = "i'm good "; // заменить текст
 console.log(example5);
