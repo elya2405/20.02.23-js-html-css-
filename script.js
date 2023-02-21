bdb2eca42ba1baf2ec4fa41e6827b73bf78bd520
kanybekova eleonora <eelya250@gmail.com>
2023-02-21 14:43:50 +0600

    brought out a paragraph on id

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log(example1);
 
 let example2 = document.querySelector("P");
 console.log(example2);
+
+let example3 = document.querySelector(".example-3");
+console.log(example3);
+
+let example4 = document.querySelector("#example-4");
+console.log(example4);
